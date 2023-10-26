b3ac0530d5d5fcb4e0a6af11865165d0cc662057
EdwardLemus <edcruzonixgo@gmail.com>
2023-10-26 15:34:17 -0600

    ejemplo de scrollConfiguration

--- lib/view/page1.dart
+++ lib/view/page1.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Ejemplo de ScrollConfiguration'),
+        ),
+        body: ScrollConfiguration(
+          behavior:
+              ScrollBehavior(), // 1. Comportamiento de desplazamiento personalizado
+          child: ListView.builder(
+            itemCount: 100,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text('Elemento $index'),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
